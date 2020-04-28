eb721df9e111518e851bb2efceba6e38cdc08f46
Evan Owen <kainosnoema@gmail.com>
2020-04-28 16:00:14 -0700

    Move table formatting into plan package
    Signed-off-by: Evan Owen <kainosnoema@gmail.com>

--- cmd/plan.go
+++ cmd/plan.go
@@ -3,9 +3,7 @@ package cmd
 import (
 	"fmt"
 	"os"
-	"strconv"
 
-	"github.com/olekukonko/tablewriter"
 	"github.com/spf13/cobra"
 
 	"github.com/kainosnoema/terracost/cli/plan"
@@ -38,45 +36,5 @@ func runPlan(cmd *cobra.Command, args []string) {
 	}
 
 	fmt.Println()
-
-	data := [][]string{}
-	hourlyTotal := 0.0
-	monthlyTotal := 0.0
-	for _, resource := range resources {
-		hourlyCost, _ := strconv.ParseFloat(resource.Price.Dimensions[0].PricePerUnit, 32)
-		monthlyCost := hourlyCost * 730
-		hourlyTotal += hourlyCost
-		monthlyTotal += monthlyCost
-		data = append(data, []string{
-			resource.Type,
-			resource.Name,
-			resource.ServiceCode,
-			resource.UsageOperation,
-			"$" + strconv.FormatFloat(hourlyCost, 'f', 3, 32),
-			"$" + strconv.FormatFloat(monthlyCost, 'f', 2, 32),
-		})
-	}
-
-	table := tablewriter.NewWriter(os.Stdout)
-	table.SetHeader([]string{
-		"Resource",
-		"Name",
-		"Service",
-		"Usage Operation",
-		"Hourly Cost",
-		"Monthly Cost",
-	})
-	table.SetFooter([]string{
-		"",
-		"",
-		"",
-		"Total",
-		"$" + strconv.FormatFloat(hourlyTotal, 'f', 3, 32),
-		"$" + strconv.FormatFloat(monthlyTotal, 'f', 2, 32),
-	})
-	table.SetAlignment(tablewriter.ALIGN_RIGHT)
-	table.SetFooterAlignment(tablewriter.ALIGN_RIGHT)
-	table.SetBorder(false)
-	table.AppendBulk(data)
-	table.Render()
+	plan.FormatTable(os.Stdout, resources)
 }
---
+++ plan/format.go
@@ -0,0 +1,53 @@
+package plan
+
+import (
+	"io"
+	"strconv"
+
+	"github.com/olekukonko/tablewriter"
+)
+
+func FormatTable(writer io.Writer, resources []Resource) {
+	data := [][]string{}
+	hourlyTotal := 0.0
+	monthlyTotal := 0.0
+
+	for _, res := range resources {
+		hourlyCost, _ := strconv.ParseFloat(res.Price.Dimensions[0].PricePerUnit, 32)
+		monthlyCost := hourlyCost * 730
+		hourlyTotal += hourlyCost
+		monthlyTotal += monthlyCost
+
+		data = append(data, []string{
+			res.Type,
+			res.Name,
+			res.ServiceCode,
+			res.UsageOperation,
+			"$" + strconv.FormatFloat(hourlyCost, 'f', 3, 32),
+			"$" + strconv.FormatFloat(monthlyCost, 'f', 2, 32),
+		})
+	}
+
+	table := tablewriter.NewWriter(writer)
+	table.SetHeader([]string{
+		"Resource",
+		"Name",
+		"Service",
+		"Usage Operation",
+		"Hourly Cost",
+		"Monthly Cost",
+	})
+	table.SetFooter([]string{
+		"",
+		"",
+		"",
+		"Total",
+		"$" + strconv.FormatFloat(hourlyTotal, 'f', 3, 32),
+		"$" + strconv.FormatFloat(monthlyTotal, 'f', 2, 32),
+	})
+	table.SetAlignment(tablewriter.ALIGN_RIGHT)
+	table.SetFooterAlignment(tablewriter.ALIGN_RIGHT)
+	table.SetBorder(false)
+	table.AppendBulk(data)
+	table.Render()
+}
